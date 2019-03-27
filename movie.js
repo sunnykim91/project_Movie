47bcb9e852cd698d78e0e1ae035ff4ea9691574c
dearjsmc4 <dearjsmc4@gmail.com>
2019-03-27 17:02:20 +0900

    feat: solving conflict

--- movie.js
+++ movie.js
@@ -5,7 +5,7 @@ function load(){
         if(Object.keys(check_m)[0] == m.innerHTML){
         $(m).parent().css('opacity', '0.2'); 
         // card-title 태그의 부모의 card-body 에 투명도
-        $(m).parent().siblings("img").css('opacity', '0.2'); 
+        $(m).parent().siblings("img").css('opacity', '0.5'); 
         // card-title 의 부모의 형제들 중 img 태그에 투명도
         $(m).parent().parent().prepend('<img src="completed2.png" class="complete-img">');
         // 포스터 사진 위에 comeplete 이미지 추가
@@ -24,7 +24,7 @@ load();
     function(){
         if ($(this).is(':checked')) {
          
-          $(this).parent().siblings().css('opacity', '0.2');
+          $(this).parent().siblings().css('opacity', '0.5');
           $(this).parent().parent().prepend('<img src="completed2.png" class="complete-img">');
           var dialog = document.querySelector('dialog');
          var showDialogButton = document.querySelector('#show-dialog');
